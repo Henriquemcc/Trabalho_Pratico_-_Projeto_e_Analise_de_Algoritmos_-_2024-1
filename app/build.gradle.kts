27e5d8fbef168afd7bdc9728f283f525981272f5
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-15 07:03:39 -0300

    Configurando o gradle para gerar o .jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,3 +34,16 @@ application {
     // Define the main class for the application.
     mainClass = "paa.tp.App"
 }
+
+tasks {
+    withType<Jar> {
+        manifest {
+            attributes["Main-Class"] = application.mainClass.get()
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+
+        configurations["compileClasspath"].forEach { file: File ->
+            from(zipTree(file.absoluteFile))
+        }
+    }
+}
