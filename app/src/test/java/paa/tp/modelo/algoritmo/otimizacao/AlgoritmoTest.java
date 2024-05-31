3876494818ae2ab36966ac6e20036836e34b9be9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-31 12:40:21 -0300

    Unificando testes de unidade em um único arquivo

--- app/src/test/java/paa/tp/modelo/algoritmo/otimizacao/AlgoritmoTest.java
+++ app/src/test/java/paa/tp/modelo/algoritmo/otimizacao/AlgoritmoTest.java
@@ -48,4 +48,27 @@ public class AlgoritmoTest extends TestCase {
         assertEquals(forcaBruta.getMelhorSolucao(), branchAndBound.getMelhorSolucao());
     }
 
+    /**
+     * Realiza o caso de teste da entrada passada pelo professor sem nenhuma restrição (distância mínima).
+     */
+    public void testExemploProfessor() {
+        final List<PontoCandidato> pontosCandidatos = new ArrayList<>();
+        pontosCandidatos.add(new PontoCandidato(1, 50, 150, 2000));
+        pontosCandidatos.add(new PontoCandidato(1, 150, 150, 1000));
+        pontosCandidatos.add(new PontoCandidato(2, 200, 180, 500));
+        pontosCandidatos.add(new PontoCandidato(2, 220, 200, 800));
+        pontosCandidatos.add(new PontoCandidato(2, 300, 150, 1000));
+        pontosCandidatos.add(new PontoCandidato(3, 100, 250, 800));
+        pontosCandidatos.add(new PontoCandidato(3, 180, 220, 500));
+        pontosCandidatos.add(new PontoCandidato(3, 150, 300, 700));
+        pontosCandidatos.add(new PontoCandidato(4, 220, 220, 400));
+        pontosCandidatos.add(new PontoCandidato(4, 300, 300, 1000));
+        final int distanciaMinima = 0;
+        final BranchAndBound branchAndBound = new BranchAndBound(pontosCandidatos, distanciaMinima);
+        branchAndBound.executar();
+        assertEquals(branchAndBound.getMelhorSolucao().getPontosCandidatosEscolhidos(), pontosCandidatos);
+        final ForcaBruta forcaBruta = new ForcaBruta(pontosCandidatos, distanciaMinima);
+        forcaBruta.executar();
+    }
+
 }
