0fdf08e3fde71cede7655890ce02f889bf8ac6bd
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-31 12:08:46 -0300

    Adicionando outro caso de teste 100% aleatório

--- app/src/test/java/paa/tp/modelo/algoritmo/otimizacao/AlgoritmoTest.java
+++ app/src/test/java/paa/tp/modelo/algoritmo/otimizacao/AlgoritmoTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import paa.tp.modelo.PontoCandidato;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -16,10 +17,24 @@ public class AlgoritmoTest extends TestCase {
     /**
      * Realiza caso de teste aleatório comparando os algoritmos Branch and Bound e Força Bruta.
      */
-    @Test
-    public void testAlgoritmos() {
+    public void testAlgoritmosAleatoriamente() {
+        testarAleatoriamente(new Random());
+    }
+
+    /**
+     * Realiza caso de teste completamente aleatório (Secure Random) comparando os algoritmos Branch and Bound e Força Bruta.
+     */
+    public void testAlgoritmosAleatoriamenteSeguro() {
+        testarAleatoriamente(new SecureRandom());
+    }
+
+    /**
+     * Teste aleatoriamente, comparando os algoritmos Branch and Bound e Força Bruta.
+     * @param random Random a ser utilizado para gerar os casos de teste.
+     */
+    private void testarAleatoriamente(final Random random) {
         final List<PontoCandidato> pontosCandidatos = new ArrayList<>();
-        final Random random = new Random();
+
         final int numeroFranquias = 10;
         final int numeroFiliaisPorFranquia = 2;
         for (int franquia = 0; franquia < numeroFranquias; franquia++)
